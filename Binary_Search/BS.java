4cc20bab422f710d199f42373c1a2ab61d8a381b
vatan-apk <vatankumarpathak9394@gmail.com>
2026-07-11 21:09:41 +0530

    Today's DSA Practice

--- Binary_Search/BS.java
+++ Binary_Search/BS.java
@@ -355,6 +355,7 @@ return sum;
 public static void main(String[] args) {
     int[] arr  = {1,2,3,4,5,6};
 System.out.println(nr(3,27));
+// test
 }
 
 
---
+++ push.bat
@@ -0,0 +1,5 @@
+@echo off
+git add .
+git commit -m "Today's DSA Practice"
+git push
+pause
