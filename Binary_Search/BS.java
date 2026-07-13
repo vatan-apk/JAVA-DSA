452264e5669c0d1a7720796e04686174f9e8ef76
vatan-apk <vatankumarpathak9394@gmail.com>
2026-07-13 09:19:42 +0530

    Today's DSA Practice

--- Binary_Search/BS.java
+++ Binary_Search/BS.java
@@ -327,6 +327,40 @@ public static int nr(int n,int m){
     return -1;
 }
 
+
+// koko eating bananas
+
+public static int MaxValue(int[] arr){
+    int maxi = Integer.MIN_VALUE;
+    for(int i=0;i<arr.length;i++){
+        maxi = Math.max(maxi,arr[i]);
+    }
+    return maxi;
+}
+
+public static int calculateTotalHours(int[] arr,int hourly){
+int totalH=0;
+int n = arr.length;
+for(int i=0;i<n;i++){
+    totalH+= Math.ceil((double)arr[i]/(double)hourly);
+}
+return totalH;
+}
+
+public static int minRatetoEatBananas(int arr[],int h){
+int low =1,high=MaxValue(arr);
+while(low<=high){
+    int mid = (low+high)/2;
+    int totalH = calculateTotalHours(arr,mid);
+    if(totalH<=h){
+        high = mid-1;
+    }else{
+        low = mid+1;
+    }
+}
+return low;
+}
+
 // Median of two sorted arrays brute force approach
 public static int msa(int[] arr1,int[] arr2){
 ArrayList<Integer> list = new ArrayList<>();
@@ -351,6 +385,66 @@ int sum = (list.get(idx1)+list.get(idx2))/2;
 return sum;
 }
 
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        if (nums1.length > nums2.length)
+            return findMedianSortedArrays(nums2, nums1);
+
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+
+        int low = 0;
+        int high = n1;
+
+        while (low <= high) {
+
+            int cut1 = (low + high) / 2;
+            int cut2 = (n1 + n2 + 1) / 2 - cut1;
+
+            int left1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            int right1 = (cut1 == n1) ? Integer.MAX_VALUE : nums1[cut1];
+
+            int left2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
+            int right2 = (cut2 == n2) ? Integer.MAX_VALUE : nums2[cut2];
+            
+            if (left1 <= right2 && left2 <= right1) {
+
+                if ((n1 + n2) % 2 == 0) {
+                    return (Math.max(left1, left2) + Math.min(right1, right2)) / 2.0;
+                } else {
+                    return Math.max(left1, left2);
+                }
+            }
+
+            else if (left1 > right2) {
+                high = cut1 - 1;
+            }
+            else {
+                low = cut1 + 1;
+            }
+        }
+        return 0.0;
+    }
+
+
+// Brute force approach for Kth element of two sorted arrays
+
+public static int KthElement(int arr1[],int[] arr2,int idx){
+    ArrayList<Integer> list = new ArrayList<>();
+int i=0;
+while(i<arr1.length){
+list.add(arr1[i]);
+i++;
+}
+int j=0;
+while(j<arr2.length){
+    list.add(arr2[j]);
+    j++;
+}
+Collections.sort(list);
+return list.get(idx);
+}
 
 public static void main(String[] args) {
     int[] arr  = {1,2,3,4,5,6};
