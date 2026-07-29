07b190144bdd1dd2654976010b3566b5136887fb
vatan-apk <vatankumarpathak9394@gmail.com>
2026-07-29 12:45:11 +0530

    Heap Trees Question solving

--- src/Heap/Sliding_Window_Max.java
+++ src/Heap/Sliding_Window_Max.java
@@ -1,45 +1,45 @@
-import java.util.*;
+import java.util.PriorityQueue;
 
-public class Sliding_Window_Max{
-static class Pair implements Comparable<Pair>{
-    int val;
-    int idx;
+public class Sliding_Window_Max {
+    static class Pair implements Comparable<Pair> {
+        int val;
+        int idx;
 
-    public Pair(int val,int idx){
-        this.val =val;
-        this.idx =idx;
-    }
-
-    @Override
-    public int compareTo(Pair p2){
-        // ascending
-        return this.val - p2.val;
-    }
-}
-    public static void main(String[] args) {
-        int arr[] = {1,3,-1,-3,5,3,6,7};
-        int k=3;
-        int res[] = new int[arr.length-k+1];
-
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
-   
-        for(int i=0;i<k;i++){
-            pq.add(new Pair(arr[i],i));
+        Pair(int val, int idx) {
+            this.val = val;
+            this.idx = idx;
         }
 
-        res[0] =pq.peek().val;
+        @Override
+        public int compareTo(Pair other) {
+            if (this.val != other.val) {
+                return Integer.compare(other.val, this.val);
+            }
+            return Integer.compare(this.idx, other.idx);
+        }
+    }
 
-        for(int i=k;i<arr.length;i++){
-            while(!pq.isEmpty() && pq.peek().idx <= i-k){
-                pq.remove();
+    public static void main(String[] args) {
+        int[] arr = {1, 3, -1, -3, 5, 3, 6, 7};
+        int k = 3;
+
+        int[] result = new int[arr.length - k + 1];
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            pq.add(new Pair(arr[i], i));
+
+            while (!pq.isEmpty() && pq.peek().idx <= i - k) {
+                pq.poll();
             }
 
-            pq.add(new Pair(arr[i],i));
-            res[i-k+1] = pq.peek().val;
+            if (i >= k - 1) {
+                result[i - k + 1] = pq.peek().val;
+            }
         }
 
-        for(int i=0;i<res.length;i++){
-            System.out.print(res[i] + " ");
+        for (int value : result) {
+            System.out.print(value + " ");
         }
     }
 }
